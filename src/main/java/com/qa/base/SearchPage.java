098594cf87c8c06e93514a0e9eee3cbcd1ca0a2e
w252625 <Harjeet.x.chatha@kp.org>
2020-04-28 15:49:13 -0700

    adding search page

---
+++ src/main/java/com/qa/base/SearchPage.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package com.qa.base;
+
+/**
+ * @author W252625
+ *
+ */
+public class SearchPage {
+
+	public void searchPage(){
+		System.out.println("print");
+	}
+
+	public void searchLogin(){
+		System.out.println("print searcg kigub");
+	}
+
+
+}
